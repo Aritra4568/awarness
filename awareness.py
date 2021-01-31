8e657b7fc8451c2023b0383b97e4dc27266735ae
Aritra Malik <asitmalik75@gmail.com>
2021-01-31 12:14:18 +0530

    first commit

---
+++ awareness.py
@@ -0,0 +1,138 @@
+import os
+import time
+print("hello world!\nI am your friend jack\nI want to talk with you\n")
+yes=input("Have you some time for this(y/n): ")
+if yes == 'y':
+    print("ok let's start")
+    print("")
+    print("Getting all quistion(s)...")
+    time.sleep(10)
+    print("Wait 5 second(s)...")
+    time.sleep(5)
+    print("1. How much old are you?")
+    a=input("ans: ")
+    print("2. What is your hobby?")
+    b=input("ans: ")
+    print("Oh so your hobby is "+b)
+    print("3. Where are you from?")
+    c=input("ans: ")
+    print("So you are from "+c+". I really love that place.")
+    if a == '10':
+        print("4. Are you in class 5?(y/n)")
+        das=input("ans: ")
+        if das == 'y':
+            print("I guessed that 😎 ")
+        if das == 'n':
+            print("Oh I need more practice!!!")
+    if a == '11':
+        print("4. Are you in class 6?(y/n)")
+        fun=input("ans: ")
+        if fun == 'y':
+            print("I guessed that 😎 ")
+        if fun == 'n':
+            print("Oh I need more practice!!!")
+    if a == '12':
+        print("4. Are you in class 7?(y/n)")
+        gol=input("ans: ")
+        if gol == 'y':
+            print("I guessed that 😎 ")
+        if gol == 'n':
+            print("Oh I need more practice!!!")
+    if a == '13':
+        print("4. Are you in class 8?(y/n)")
+        kal=input("ans: ")
+        if kal == 'y':
+            print("I guessed that 😎 ")
+        if kal == 'n':
+            print("Oh I need more practice!!!")
+    if a == '14':
+        print("4. Are you in class 9?(y/n)")
+        jule=input("ans: ")
+        if jule == 'y':
+            print("I guessed that 😎 ")
+        if jule == 'n':
+            print("Oh I need more practice!!!")
+    if a == '15':
+        print("4. Are you in class 10?(y/n)")
+        fox=input("ans: ")
+        if fox == 'y':
+            print("I guessed that 😎 ")
+        if fox == 'n':
+            print("Oh I need more practice!!!")
+    if a == '16':
+        print("4. Are you in class 11?(y/n)")
+        uno=input("ans: ")
+        if uno == 'y':
+            print("I guessed that 😎 ")
+        if uno == 'n':
+            print("Oh I need more practice!!!")
+    if a == '17':
+        print("4. Are you in class 12?(y/n)")
+        drs=input("ans: ")
+        if drs == 'y':
+            print("I guessed that 😎 ")
+        if drs == 'n':
+            print("Oh I need more practice!!!")
+    if a == '18':
+        print("4. Are you in college?(y/n)")
+        homie=input("ans: ")
+        if homie == 'y':
+            print("I guessed that 😎 ")
+        if homie == 'n':
+            print("Oh I need more practice!!!")
+    print("5. What is your name?")
+    d=input("ans: ")
+    print("So your name is "+d)
+    print("6. Have you ever faced any hacker before?")
+    e=input("ans: ")
+    if e == 'n':
+        print("So then\nMeet Jack or I say Me!! --The ethical hacker.")
+    if e == 'y':
+        print("Then I want to introduce myself.\nI am Jack--The ethical hacker.")
+    print("I am going to upload all of your important files in deep web.")
+    time.sleep(2)
+    print("Shall I begin")
+    time.sleep(10)
+    print("I think ")
+    time.sleep(2)
+    print("!!! YES !!!")
+    time.sleep(5)
+    os.system("clear")
+    print("Opening deep web...")
+    time.sleep(5)
+    print("Opening SOFTeror website...")
+    time.sleep(5)
+    print("Getting all the information...")
+    time.sleep(5)
+    print("Checking all the imformation...")
+    print("All informations are: ")
+    print("entity's age: "+a)
+    print("entity's hobby: "+b)
+    time.sleep(2)
+    print("entity's location: "+c)
+    print("entity's name: "+d)
+    time.sleep(0.7)
+    print("Completed")
+    time.sleep(0.6)
+    print("Reveiwing...")
+    time.sleep(1)
+    print(a)
+    print(b)
+    time.sleep(2)
+    print(c)
+    print(d)
+    time.sleep(4)
+    print("Uploaded.")
+    time.sleep(12)
+    print("Sorry\nI am just joking.")
+    time.sleep(10)
+    print("Your privacy is yours.\nAny other besides you have no rights to use them(Including me).")
+    time.sleep(10)
+    print("I am telling you that in this century we need to aware of being hacked.")
+    time.sleep(10)
+    print("So be safe and be happy 😄 ")
+    time.sleep(5)
+    exit()
+if yes == 'n':
+    print("never mind.")
+    exit()
